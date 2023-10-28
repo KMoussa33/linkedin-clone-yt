25389f13fb2846da0d0c07f3d4ae28661c27f8ce
KMoussa33 <kelsey.moussa@gmail.com>
2023-10-28 13:19:13 -0400

    finished header & beginning left sidebar

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
 import logo from './linkedin.png'
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+
 
 
 function Header() {
@@ -18,7 +25,14 @@ function Header() {
         </div>
 
         <div className="header__right">
-
+            <HeaderOption Icon={HomeIcon} title='Home' />
+            <HeaderOption Icon={SupervisorAccountIcon}title='My Network' />
+            <HeaderOption Icon={BusinessCenterIcon}title='Jobs' />
+            <HeaderOption Icon={ChatIcon}title='Messaging' />
+            <HeaderOption Icon={NotificationsIcon}title='Notifications' />
+            <HeaderOption  
+              avatar='https://media.licdn.com/dms/image/D5635AQHz7GThSFdoDg/profile-framedphoto-shrink_200_200/0/1691249002814?e=1699120800&v=beta&t=lTZ7jiZw_jtUx4hfj80QZquuSjGTp8-GzC1xN1nsgv8' title='me' 
+            />
         </div>
     </div>
   )
