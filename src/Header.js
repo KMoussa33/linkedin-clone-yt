079e91a6859081156aeb0d188f2f0db803dd0df2
KMoussa33 <kelsey.moussa@gmail.com>
2023-10-28 12:25:41 -0400

    css

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import logo from './linkedin.png'
+
 
 function Header() {
   return (
     <div className='header'>
-        <h1>This is the header</h1>
 
         <div className="header__left">
-            <img src="" alt="" />
+            <img src={logo} alt="" />
 
             <div className="header__search">
-              {/* SearchIcon */}
+              <SearchIcon />
               <input type="text" />
             </div>
         </div>
