49a7249905bdd41f13024eb1763d0e17d7c1d5c4
KMoussa33 <kelsey.moussa@gmail.com>
2023-11-01 11:12:43 -0400

    Post & Feed

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Feed.css'
-import CreateIcon from '@mui/icons-material/Create';
 import InputOption from './InputOption';
+import CreateIcon from '@mui/icons-material/Create';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
+import Post from './Post';
+
 
 function Feed() {
+    const [posts, setPosts] = useState([])
+
+    const sendPost = (e) => {
+        e.preventDefault()
+    }
+
   return (
     <div className='feed'>
         <div className="feed__inputContainer">
@@ -11,13 +23,26 @@ function Feed() {
                 <CreateIcon />
                 <form>
                     <input type="text" />
-                    <button type='submit'>Send</button>
+                    <button onClick={sendPost} type='submit'>Send</button>
                 </form>
             </div>
             <div className="feed__inputOptions">
-                <InputOption />
+                <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9'/>
+                <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E'/>
+                <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD'/>
+                <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E'/>
             </div>
         </div>
+
+        {/* Posts */}
+        {posts.map((post) => {
+            <Post />
+        })}
+        <Post  
+            name="Kelsey Moussa" 
+            description='This is a test' 
+            message='Wow this worked' 
+        />
     </div>
   )
 }
